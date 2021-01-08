69395819d969870d2dc1af5372570fc7e243f2cc
v_weijiemo <v_weijiemo@tencent.com>
2021-01-08 19:09:46 +0800

    课程分类

--- vue-admin-template-master/src/views/edu/teacher/save.vue
+++ vue-admin-template-master/src/views/edu/teacher/save.vue
@@ -24,6 +24,30 @@
         <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
       </el-form-item>
       <!-- 讲师头像：TODO -->
+      <el-form-item label="讲师头像">
+
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+
+        <!--
+    v-show：是否显示上传组件
+    :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+    :url：后台上传的url地址
+    @close：关闭上传组件
+    @crop-upload-success：上传成功后的回调 -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/edu/oss/avatar'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+      </el-form-item>
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
       </el-form-item>
@@ -33,22 +57,55 @@
 
 <script>
 import teacherApi from '@/api/teacher/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
 export default {
+  components: {ImageCropper, PanThumb},
   data(){
     return{
       teacher:{
-
+        name: '',
+        sort: 0,
+        level: 1,
+        career: '',
+        intro: '',
+        avatar: ''
       },
+      //上传弹框组件是否显示
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      BASE_API:process.env.BASE_API,
+
       saveBtnDisabled: false // 保存按钮是否禁用,
     }
   },
   created() {
-    if(this.$route.params && this.$route.params.id){
-      const id = this.$route.params.id
-      this.getInfo(id)
+    this.init()
+  },
+  watch:{  //监听
+    $route(to,from) {   //路由发生变化,方法就会执行
+      this.init()
     }
   },
   methods:{
+    close(){//关闭上传弹框
+      this.imagecropperShow= false
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
+    cropSuccess(data){//上传成功
+      //上传之后返回图片地址
+      this.teacher.avatar = data.url
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
+    init(){
+      if(this.$route.params && this.$route.params.id){
+        const id = this.$route.params.id
+        this.getInfo(id)
+      }else {
+        this.teacher = {}
+      }
+
+    },
     getInfo(id){
       teacherApi.getTeacherInfo(id).then(response=>{
         this.teacher = response.data.teacher
